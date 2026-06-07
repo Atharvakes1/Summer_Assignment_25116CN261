ebc1b5f57b22bee14e84f6b8a4acdb677bf776f4
Atharva Vaish <atharvavaish@Atharvas-MacBook-Air.local>
2026-06-07 19:41:18 +0530

    Uploading day by day work

--- Day-9/Q33.java
+++ Day-9/Q33.java
@@ -1,3 +1,14 @@
-public class Q33 {
-    
+class Q33
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+            {
+                for(int j=5;j>=i;j--)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+    }
 }
